20714df4e8d85e44287e78656c486964872fa324
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-05-08 16:40:07 +0600

    add new user add modal

--- src/app/lib/actions.js
+++ src/app/lib/actions.js
@@ -1,21 +1,3 @@
-// import { revalidatePath } from "next/cache";
-
-// export const deleteUser = async(userId) => {
-//     'use server';
-
-//     const res = await fetch(`http://localhost:5000/users/${userId}`, {
-//         method: 'DELETE'
-//     });
-//     const data = res.json();
-//     console.log('after delete', data);
-    
-//     // TODO: Revalidate cache
-//     if(data.deletedCount > 0){
-//         revalidatePath('/users');
-//     }
-//     return data;
-// };
-
 
 import { revalidatePath } from "next/cache";
 
--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,17 +1,21 @@
+import AddUserModal from "../components/AddUserModal";
 import UsersTable from "../components/UsersTable";
 import { deleteUser } from "../lib/actions";
 import { getUsers } from "../lib/data";
 
-const UsersPage = async() => {
+const UsersPage = async () => {
 
     const users = await getUsers();
     // console.log(users);
 
     return (
         <div>
-            <h2>Users : {users.length}</h2>
+            <div className="flex justify-between items-center">
+                <h2>Users : {users.length}</h2>
+                <AddUserModal />
+            </div>
             <div>
-                <UsersTable users={users} deleteUserAction = {deleteUser} />
+                <UsersTable users={users} deleteUserAction={deleteUser} />
             </div>
         </div>
     );
